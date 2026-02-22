c454b83ff00172d64ea059385014632cd6a350b3
Hafsa El Akhdar <h.hafsaelakhdar@gmail.com>
2026-02-22 17:14:49 +0100

    Implement login and register functions . Add credentials to .env file

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,26 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./src/config/db.js";
+import connectCloudinary from "./src/config/cloudinary.js";
+import userRouter from "./src/routes/userRoutes.js";
+import productRouter from "./src/routes/productRoutes.js";
+
 
 // App Config
 
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary()
 
 // middlewares
 app.use(express.json());
 app.use(cors());
 
 // api endpoints
+app.use("/api/user", userRouter)
+app.use("/api/product", productRouter)
 
 app.get("/", (req, res) => {
   res.send("api working");
---
+++ Backend/src/controllers/productController.js
@@ -0,0 +1,14 @@
+import userModel from "../models/Product.js";
+
+const addProduct = async (req, res) => {};
+const listProduct = async (req, res) => {};
+const removeProduct = async (req, res) => {};
+const singleProduct = async (req, res) => {};
+
+
+export{
+    addProduct,
+    listProduct,
+    removeProduct,
+    singleProduct
+}
