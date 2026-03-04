bd0d037eb107eef71654f4e2e68c25ddb04f70cc
Hafsa El Akhdar <h.hafsaelakhdar@gmail.com>
2026-03-04 13:41:25 +0100

    update logo

--- Frontend/src/components/BestSeller.jsx
+++ Frontend/src/components/BestSeller.jsx
@@ -1,32 +1,38 @@
-import React, {useEffect, useContext, useState} from 'react';
-import { ShopContext } from '../context/shopContext';
-import Title from './Title';
-import ProductItem from './ProductItem';
+import React, { useEffect, useContext, useState } from "react";
+import { ShopContext } from "../context/shopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
 
 const BestSeller = () => {
-    const {products} = useContext(ShopContext);
-    const [bestSeller, setBestSeller] = useState([]);
-    useEffect(() => {
-
-        const bestSellerProducts = products.filter(product => product.bestseller);
-        setBestSeller(bestSellerProducts.slice(0, 5));
-
-    }, [products]);
+  const { products } = useContext(ShopContext);
+  const [bestSeller, setBestSeller] = useState([]);
+  useEffect(() => {
+    const bestSellerProducts = products.filter((product) => product.bestseller);
+    setBestSeller(bestSellerProducts.slice(0, 5));
+  }, [products]);
 
   return (
-    <div className='my-10'>
-        <div className="text-center py-8 text-3xl">
-            <Title text1={"BEST"} text2={"SELLER"}/>
-            <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">Discover our best-selling products and exclusive collections</p>
-        </div>
-        {/* Render best seller products */}
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-            {bestSeller.map((product, index) => (
-                <ProductItem key={index} id={product._id} name={product.name} image={product.image} price={product.price} />
-            ))}
-        </div>
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"BEST"} text2={"SELLER"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          Discover our best-selling products and exclusive collections
+        </p>
+      </div>
+      {/* Render best seller products */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {bestSeller.map((product, index) => (
+          <ProductItem
+            key={index}
+            id={product._id}
+            name={product.name}
+            image={product.image}
+            price={product.price}
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BestSeller
+export default BestSeller;
